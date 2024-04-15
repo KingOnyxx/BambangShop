426b80c583591a6eae478f52f5e2455a8d93cec7
Clarence Grady <clarence.grady@ui.ac.id>
2024-04-15 21:52:27 +0700

    Implement delete function in Subscriber repository

--- src/repository/subscriber.rs
+++ src/repository/subscriber.rs
@@ -1,5 +1,3 @@
-use std::ops::Sub;
-
 use dashmap::DashMap;
 use lazy_static::lazy_static;
 use crate::model::subscriber::Subscriber;
@@ -32,5 +30,17 @@ impl SubscriberRepository {
             .iter()
             .map(|f| f.value().clone())
             .collect();
+    }
+
+    pub fn delete(product_type: &str, url: &str) -> Option<Subscriber> {
+        if SUBSCRIBERS.get(product_type).is_none() {
+            SUBSCRIBERS.insert(String::from(product_type), DashMap::new());
+        }
+        let result = SUBSCRIBERS.get(product_type).unwrap()
+            .remove(url);
+        if !result.is_none() {
+            return Some(result.unwrap().1);
+        }
+        return None;
     }
 }
