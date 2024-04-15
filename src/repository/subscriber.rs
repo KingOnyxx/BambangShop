2d660a42eda78ec169221e84e103a1ac892c8e77
Clarence Grady <clarence.grady@ui.ac.id>
2024-04-15 21:47:40 +0700

    Implement add function in Subscriber repository

--- src/repository/subscriber.rs
+++ src/repository/subscriber.rs
@@ -1,3 +1,5 @@
+use std::ops::Sub;
+
 use dashmap::DashMap;
 use lazy_static::lazy_static;
 use crate::model::subscriber::Subscriber;
@@ -10,5 +12,14 @@ lazy_static! {
 pub struct SubscriberRepository;
 
 impl SubscriberRepository {
-    
+    pub fn add(product_type: &str, subscriber: Subscriber) -> Subscriber {
+        let subscriber_value = subscriber.clone();
+        if SUBSCRIBERS.get(product_type).is_none() {
+            SUBSCRIBERS.insert(String::from(product_type), DashMap::new());
+        }
+
+        SUBSCRIBERS.get(product_type).unwrap()
+            .insert(subscriber.url.clone(), subscriber_value.clone());
+        return subscriber;
+    }
 }
